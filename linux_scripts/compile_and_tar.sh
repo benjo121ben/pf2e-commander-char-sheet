bcc62a9010e7f7b2b3050e824e002e323d83ad46
benjo121ben <benjamin.wenger00@gmail.com>
2024-10-09 15:34:11 +0200

    updated linux files

--- linux_scripts/compile_and_tar.sh
+++ linux_scripts/compile_and_tar.sh
@@ -2,13 +2,17 @@
 cp Cargo_linux.toml Cargo.toml &&
 cargo leptos build --release &&
 t_dir=pf2e-sheet &&
-mkdir -p $t_dir &&
-mkdir -p $t_dir/saves &&
-cp -r target/site $t_dir/site &&
+rm -r $t_dir
+mkdir $t_dir &&
+mkdir $t_dir/saves &&
+mkdir $t_dir/dev_saves &&
+mkdir $t_dir/resources &&
+cp -r target/site/. $t_dir/site/ &&
+mv $t_dir/site/pkg/pf2e-char-sheet.wasm $t_dir/site/pkg/pf2e-char-sheet_bg.wasm &&
+cp -r resources/. $t_dir/resources/ &&
+cp -r saves/. $t_dir/dev_saves/ &&
 cp target/armv7-unknown-linux-gnueabihf/release/pf2e-char-sheet $t_dir/pf2e-char-sheet &&
 cp Cargo_deploy.toml $t_dir/Cargo.toml &&
-cp -r resources $t_dir/resources &&
-cp -r saves $t_dir/dev_saves &&
 tar czf pf2e-app.tar.gz -C $t_dir . &&
 echo project built and zipped into tar file
 
--- linux_scripts/deploy_char_sheet.sh
+++ linux_scripts/deploy_char_sheet.sh
@@ -1,7 +1,10 @@
+sudo systemctl stop char-sheet.service
 base_dir=~/programs/pf2e-char-sheet &&
+rm -r $base_dir/site
 mkdir -p $base_dir &&
 mv ./pf2e-app.tar.gz $base_dir/pf2e-app.tar.gz &&
 cd $base_dir &&
 tar -xf pf2e-app.tar.gz &&
 rm pf2e-app.tar.gz &&
-./pf2e-char-sheet
+sudo systemctl start char-sheet.service &&
+echo done with deploy
