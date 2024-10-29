1a0f32ba8f1593dba6f10f9711c0399b8ae051ce
Benjamin Wenger <benjamin.wenger@unisys.com>
2024-10-29 17:03:15 +0100

    WIP_conditions

--- src/char_data/conditions.rs
+++ src/char_data/conditions.rs
@@ -1,43 +1,63 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum BonusPenaltyCalcType {
+    Fixed,
+    Calculated,
+    ConditionLevel
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum BonusPenaltyType {
+    Circumstance,
+    Status,
+    Item,
+    Untyped
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Condition {
     pub name: String,
+
     pub description: String,
-    #[serde(rename = "override")]
+
     #[serde(default)]
-    pub override_field: Vec<String>,
+    pub override_other: Vec<String>,
+
     #[serde(default)]
     pub forced_conditions: Vec<ForcedCondition>,
+
     #[serde(default)]
-    pub penalty: Vec<Penalty>,
+    pub bonus_penalty: Vec<BonusPenalty>,
+
     #[serde(default)]
     pub has_value: bool,
+
     #[serde(default)]
     pub added_on_remove: Vec<String>,
+
     pub increase_on_gain_by: Option<String>,
+
     #[serde(default)]
     pub added_on_gain: Vec<String>,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive( Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ForcedCondition {
     pub name: String,
     pub value: Option<i32>,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Penalty {
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct BonusPenalty {
     pub selector: Vec<String>,
-    pub amount: Option<Amount>,
+    pub amount: BonusPenaltyAmount,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Amount {
-    #[serde(rename = "calc_type")]
-    pub calc_type: String,
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct BonusPenaltyAmount {
+    pub calc_type: BonusPenaltyCalcType,
     pub value: Option<i32>,
-    #[serde(rename = "penalty_type")]
-    pub penalty_type: Option<String>,
+    pub penalty_type: Option<BonusPenaltyType>,
 }
 
