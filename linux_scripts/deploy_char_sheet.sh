1adc30c13039582c24d7b7f2d6b39f21ec44cad2
benjo121ben <benjamin.wenger00@gmail.com>
2024-11-03 17:23:54 +0100

    adjusted deployment to copy font

--- linux_scripts/deploy_char_sheet.sh
+++ linux_scripts/deploy_char_sheet.sh
@@ -5,6 +5,8 @@ mkdir -p $base_dir &&
 mv ./pf2e-app.tar.gz $base_dir/pf2e-app.tar.gz &&
 cd $base_dir &&
 tar -xf pf2e-app.tar.gz &&
+mkdir -p $base_dir/saves &&
+sudo cp /usr/share/fonts/custom/Pathfinder-Icons.tff $base_dir/site/pkg/Pathfinder-Icons.tff &&
 rm pf2e-app.tar.gz &&
 sudo systemctl start char-sheet.service &&
 echo done with deploy
